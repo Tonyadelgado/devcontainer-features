2ce5cbce0e6f3d577d2eddcd338df8664e014d1e
Chuck Lantz <chuck_lantz@hotmail.com>
2022-01-30 05:31:54 +0000

    Testing and debugging

--- buildpack/scripts/prep.sh
+++ buildpack/scripts/prep.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
+target_folder="${1:-out}"
 
-./scripts/compile.sh
+mkdir -p "${target_folder}"
 
-cp -rf ../features out/
-cp -rf ../common out/
-cp -f ../features.json out/
-cp -f ../buildpack-settings.json out/
-cp -rf assets/* out/
+cp -rf ../features "${target_folder}"/
+cp -rf ../common "${target_folder}"/
+cp -f ../features.json "${target_folder}"/
+cp -f ../buildpack-settings.json "${target_folder}"/
+cp -rf assets/* "${target_folder}"/
--- buildpack/test/test.sh
+++ buildpack/test/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
-./scripts/prep.sh
+./scripts/build.sh
 
 buildpack_root="$(cd "out" && pwd)"
 pack build -v test_image \
     -e "BP_CONTAINER_FEATURE_PACKCLI=true" \
     -e "BP_CONTAINER_FEATURE_GOOGLECHROME=true" \
-    -e "BP_DEV_CONTAINER_BUILD=true" \
+    -e "BP_DEV_CONTAINER_BUILD_CONTEXT=devcontainer" \
     --pull-policy if-not-present \
     --builder ghcr.io/chuxel/devcontainer-features/builder-devcontainer \
     --trust-builder \
