09b8b93844bf410cfec8ae7aa2244b826f6e6f56
Chuck Lantz <chuck_lantz@hotmail.com>
2022-02-10 19:34:25 +0000

    Fix

--- buildpack/scripts/compile.sh
+++ buildpack/scripts/compile.sh
@@ -7,6 +7,7 @@ build_api_binary()
     source="$1"
     binary_name="${2:-"${source}"}"
     cd "${root_path}/${source}"
+    go get
     GOARCH=amd64 GOOS=linux go build -a -o "${root_path}/dist/${binary_name}-linux-amd64"
     GOARCH=arm64 GOOS=linux go build -a -o "${root_path}/dist/${binary_name}-linux-arm64"
     GOARCH=amd64 GOOS=darwin go build -a -o "${root_path}/dist/${binary_name}-darwin-amd64"
