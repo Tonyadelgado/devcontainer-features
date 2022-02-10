05f335a20e69e9f214f3c1fa942a85eb96536d28
Chuck Lantz <chuck_lantz@hotmail.com>
2022-02-10 03:47:04 +0000

    Feature fixes

--- test/test-full-builder.sh
+++ test/test-full-builder.sh
@@ -2,6 +2,11 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+clear_cache_flag=""
+if [ "${1:-false}" = "true" ]; then
+    clear_cache_flag="--clear-cache"
+fi
+
 ../scripts/create-full-builders.sh
 cd assets
-pack build test_image --pull-policy if-not-present --trust-builder  --builder ghcr.io/chuxel/devcontainer-features/builder-devcontainer
+pack build test_image "${clear_cache_flag}" --pull-policy if-not-present --trust-builder  --builder ghcr.io/chuxel/devcontainer-features/builder-devcontainer
