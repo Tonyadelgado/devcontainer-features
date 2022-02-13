9e3706faf24b80ee06cf94fe349d331b54d8aa85
Chuck Lantz <chuck_lantz@hotmail.com>
2022-02-13 02:18:59 +0000

    Basic semver parsing

--- prodpacks/test/test-prod-buildpacks.sh
+++ prodpacks/test/test-prod-buildpacks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-devpacker_dir="${script_dir}/../../buildpack"
+devpacker_dir="${script_dir}/../../devpacker"
 devpack_root="${devpacker_dir}"/test/out/buildpack
 prodpack_root="${script_dir}/.."
 
-"${devpacker_dir}"/scripts/compile.sh
-"${devpacker_dir}"/devpacker "${script_dir}"/../.. "${devpack_root}"
+"${devpacker_dir}"/scripts/compile.sh true
+"${devpacker_dir}"/devpacker generate "${script_dir}"/../.. "${devpack_root}"
 
 "${script_dir}"/../../builders/create-builders.sh empty
 cd "${script_dir}/test-project"
